127d2376a37e03564540630b5c7cdd47c3bd0ff7
ESMihaiu <stefaneduardmihaiu@gmail.com>
2025-03-05 11:39:37 +0100

    Remove broken dependency optimization

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['../back_end/src']
-  },
   server: {
     cors: true
   }
